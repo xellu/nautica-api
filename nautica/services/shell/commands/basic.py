85e173683a3dfaa029a7f55d0e0d371cdfeece7d
Xel Lu <xellu@duck.com>
2025-10-11 18:35:23 +0700

    shell commands

--- nautica/services/shell/commands/basic.py
+++ nautica/services/shell/commands/basic.py
@@ -1,5 +1,22 @@
-from ..shared import ShellBus
 from ..descriptor import ShellCommand
+from ..shared import logger
+
+@ShellCommand("help", "Shows command list or usage guides", "help [command]")
+def help(command=None):
+    if not command:
+        logger.info("Command List:      <> - Required | [] - Optional")
+        for name, cmd in ShellCommand.commands.items():
+            logger.info(f"  {cmd['usage']} - {cmd['description']}")
+        
+        return
+
+    cmd = ShellCommand.commands.get(command)
+    if not cmd:
+        logger.warn("Command not found")
+    
+    logger.info(f"About {str(command).capitalize()}:")
+    for k, v in cmd.items():
+        logger.info(f"  {k}: {v}")
 
 @ShellCommand("stop", "Stops the server", "stop [--force]")
 def shutdown(force=False):
